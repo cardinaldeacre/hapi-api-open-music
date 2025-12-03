008d4d8688c863a4be67cac2e7d1ce8a33bfac53
cardinaldeacre <cardinaldeacre@gmail.com>
2025-12-03 13:42:41 +0700

    create handlerr

---
+++ src/api/users/handler.js
@@ -0,0 +1,26 @@
+const autoBind = require('auto-bind');
+
+class UserHandler {
+    constructor(service, validator) {
+        this._service = service;
+        this._validator = validator;
+
+        autoBind(this);
+    }
+
+    async postUserHandler(request, h) {
+        this._validator.validateUserPayload(request.payload);
+
+        const { username, password, fullname } = request.payload;
+        const userId = await this._service.addUser({ username, password, fullname });
+
+        return h.response({
+            status: 'succss',
+            data: {
+                userId,
+            }
+        }).code(201);
+    }
+}
+
+module.exports = UserHandler;
